c926e3bdbfc9fae9b66bdc31f8fc269e9e2a7b58
Igor Antun <igorantunc@hotmail.com>
2024-05-01 21:38:14 +0000

    refactor: Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,26 @@
 import { Link } from "@nextui-org/link";
 
 import { siteConfig } from "@/config/site";
+import { Divider } from "@nextui-org/divider";
 
 const Footer = () => (
-  <footer className="flex flex-col">
-    {Object.entries(siteConfig.links).map(([name, href]) => (
-      <Link
-        isExternal
-        key={name}
-        href={href}
-        showAnchorIcon
-        color="success"
-        className="w-min ml-4 mt-2"
-      >
-        {name}
-      </Link>
-    ))}
+  <footer>
+    <Divider className="md:hidden mt-4 mb-2" />
+    <div className="md:px-16 flex justify-center md:items-end md:flex-col gap-4">
+      {Object.entries(siteConfig.links).map(([name, href]) => (
+        <Link
+          isExternal
+          key={name}
+          href={href}
+          showAnchorIcon
+          color="secondary"
+          className="md:pr-2 md:mr-1"
+        >
+          {name}
+        </Link>
+      ))}
+    </div>
+    <Divider className="md:hidden mt-2" />
   </footer>
 );
 

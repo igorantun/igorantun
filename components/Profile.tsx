02749335818481e9523ea2e2bc2f030abcc90be6
Igor Antun <igorantunc@hotmail.com>
2024-05-01 02:30:29 +0000

    refactor: Profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,30 +1,42 @@
-import { Image } from "@nextui-org/image";
-import { Divider } from "@nextui-org/divider";
-import { Spacer } from "@nextui-org/spacer";
 import { Chip } from "@nextui-org/chip";
+import { Image } from "@nextui-org/image";
+import { Spacer } from "@nextui-org/spacer";
+import { Divider } from "@nextui-org/divider";
 
 import { CheckIcon } from "@/icons/Check";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
-const Title = () => (
+const NameAndTitle = () => (
   <div>
-    <h1 className="font-serif font-bold text-4xl/8">Igor Antun</h1>
     <p className="font-mono text-gray-500">@igorantun</p>
-    <Spacer y={2} />
-    <p className="text-base">Senior Software Engineer</p>
-    <Chip
-      startContent={<CheckIcon size={18} />}
-      size="sm"
-      color="success"
-      variant="flat"
-      classNames={{ content: "font-bold uppercase" }}
-    >
-      Available for hire
-    </Chip>
+    <h1 className="font-serif font-bold text-4xl/6 underline decoration-green-400 decoration-2 decoration-wavy underline-offset-4">
+      Igor Antun
+    </h1>
+    <Spacer y={4} />
+    <p className="text-xl">Senior Software Engineer</p>
+  </div>
+);
+
+const Information = () => (
+  <div>
+    <NameAndTitle />
+    <div className="flex gap-2">
+      <Chip
+        size="sm"
+        variant="flat"
+        color="success"
+        startContent={<CheckIcon size={18} />}
+        classNames={{ content: "font-bold uppercase" }}
+      >
+        Available for hire
+      </Chip>
+      <ThemeSwitcher />
+    </div>
   </div>
 );
 
 const Profile = () => (
-  <div className="flex h-24 items-center space-x-4">
+  <div className="flex justify-center md:justify-start items-center space-x-4">
     <Image
       width={128}
       height={128}
@@ -34,8 +46,8 @@ const Profile = () => (
       alt="Igor Antun"
       src="/images/profile.jpg"
     />
-    <Divider orientation="vertical" />
-    <Title />
+    <Divider className="h-24" orientation="vertical" />
+    <Information />
   </div>
 );
 
